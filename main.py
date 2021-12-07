4533eee51b0864add9e019463fc833a168df0499
positronic-flex <e.makrushin15@gmail.com>
2021-12-07 20:17:25 +0300

    Merge branch 'master' of https://github.com/Kadetino/KSR-Bot

--- main.py
+++ main.py
@@ -29,20 +29,52 @@ async def send_Embed(ctx, event_name="NaN - Event Name", event_description="NaN"
 
 
 @bot.command(pass_context=True)
-async def extract(ctx, img_url):
-    """"help"""
+async def extract_attach(ctx):
+    # Получить изображение как вложенный файл
+    try:
+        img_url = ctx.message.attachments[0]
 
-    # Достать картинку
-    response = requests.get(img_url)
-    img = Image.open(io.BytesIO(response.content))
-    # Достать текст
-    pytesseract.pytesseract.tesseract_cmd = config.tesseract_cmd_path
-    text = pytesseract.image_to_string(img)
-    # Логирование
-    with open('log.txt', 'at') as log_file:
-        log_file.write("Message:" + ctx.message.content)
-        log_file.write("\nOutput:\n" + text + "\n------------------\n")
-    await ctx.message.channel.send(text)
+    except Exception:
+        await ctx.send('Error: no attached images')
+
+    else:
+        img = Image.open(io.BytesIO(await img_url.read()))
+
+        # Обработка текста
+        pytesseract.pytesseract.tesseract_cmd = config.tesseract_cmd_path
+        text = pytesseract.image_to_string(img)
+
+        await ctx.message.channel.send(text)
+
+@bot.command(pass_context=True)
+async def extract_url(ctx, *img_urls):
+    # Получить изображение по ссылке
+    for url in img_urls:
+        try:
+            response = requests.get(url)
+        
+        except Exception:
+            current_output = ''
+            if(len(img_urls)>1):
+                current_output =  f"{img_urls.index(url)+1}) Error: can't recognize the photo" 
+            else:
+                current_output = "Error: can't recognize the photo"
+            await ctx.send(current_output)
+
+        else:
+            img = Image.open(io.BytesIO(response.content))
+
+            # Обработка текста
+            pytesseract.pytesseract.tesseract_cmd = config.tesseract_cmd_path
+            text = pytesseract.image_to_string(img)
+
+            await ctx.message.channel.send(text)
+
+# Если оставляем логирование, то лучше оформить как вложенный декоратор
+'''with open('log.txt', 'at') as log_file:
+    log_file.write("Message:" + ctx.message.content)
+    log_file.write("\nOutput:\n" + text + "\n------------------\n")
+'''
 
 
 @bot.command()
