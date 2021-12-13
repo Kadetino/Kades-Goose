116c04f700471ddcc746434b084000d451342741
Kadetino <89729954+Kadetino@users.noreply.github.com>
2021-12-13 20:09:43 +0300

    PEP

--- database.py
+++ database.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 df = pd.read_csv('CSV bits\\data.csv')
 
+
 def find_event(image_output):
     for event_index in range(len(df.index)):
-        if(df['Event'][event_index] in image_output):
-            return (df['Event'][event_index], df['Description'][event_index])
+        if df['Event'][event_index] in image_output:
+            return df['Event'][event_index], df['Description'][event_index]
     return False
