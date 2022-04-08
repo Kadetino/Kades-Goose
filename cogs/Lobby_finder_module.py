4e3b0edc366e21d32feda5290fc3bfd91dfc7531
Kade <89729954+Kadetino@users.noreply.github.com>
2022-04-09 00:49:44 +0300

    Prototype

--- cogs/Lobby_finder_module.py
+++ cogs/Lobby_finder_module.py
@@ -1,7 +1,7 @@
 import discord  # Discord API wrapper
 from discord.ext import commands  # Discord BOT
 import sqlite3 as sl  # SQLite database
-from dateutil import parser  # Epoch time converter
+from dateutil import parser, tz  # Epoch time converter
 
 import config  # Global settings
 
@@ -58,12 +58,11 @@ class Lobby:  # needs to be in non-cog file or smth
         print(f"Description: {self.desc}")
 
 
-
 class LobbyFinderModule(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
+    @commands.command(name="Команда", pass_context=True)
     @commands.cooldown(1, config.cd_commands, commands.BucketType.guild)
     @commands.guild_only()
     async def temp(self, ctx: commands.Context):
@@ -76,8 +75,31 @@ class LobbyFinderModule(commands.Cog):
     @commands.guild_only()
     async def epoch_test(self, ctx: commands.Context, date: str):
         """Converts string to epoch - maybe store start as epoch (int) and not str?"""
-        epoch = parser.parse(date).timestamp()
+        timezone = {"CET": tz.gettz('EU/Central')}  # not sure if it works
+        epoch = parser.parse(date, tzinfos=timezone).timestamp()
         return await ctx.reply(f"<t:{int(epoch)}:R>")
+
+    @commands.command(pass_context=True)
+    @commands.cooldown(1, config.cd_commands, commands.BucketType.guild)
+    @commands.guild_only()
+    async def create_lobby(self, ctx: commands.Context, lobby_name: str, lobby_start: str, invite_link: str, guild: int, desc: str, author: discord.Member = None, ):
+        # User-host name
+        if author is None:
+            author = ctx.author
+        # Start date
+        timezone = {"CET": tz.gettz('EU/Central')}  # not sure if it works
+        epoch = parser.parse(lobby_start, tzinfos=timezone).timestamp()
+        # Invite link
+        lobby_embed = discord.Embed(title=lobby_name, colour=discord.Colour.gold())
+        lobby_embed.add_field(name=f"`Hosted by:`", value=f"<@{author.id}>")
+        lobby_embed.add_field(name="`Start:`", value=f"<t:{int(epoch)}:R>\n<t:{int(epoch)}:f>")
+        lobby_embed.add_field(name="`Discord invite:`", value=invite_link)
+        lobby_embed.add_field(name="`Guild:`", value=ctx.guild)
+        lobby_embed.add_field(name="`Schedule:`", value="Tuesdays, 19:00 - 19:01")
+        lobby_embed.add_field(name="`Players to start:`", value="9")
+        lobby_embed.add_field(name="`Description:`", value=desc, inline=False)
+        lobby_embed.set_thumbnail(url=ctx.guild.icon_url)
+        return await ctx.reply(embed=lobby_embed)
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import discord  # Discord API wrapper
 from discord.ext import commands  # Discord BOT
 import os  # add all Discord.py cogs from directory
-import aiohttp  # For direct API requests - duel cog
-import warnings  # For direct API requests - duel cog
+import aiohttp  # For direct API requests and webhooks
+import warnings  # For direct API requests and webhooks
 
 import config  # Global settings
 
 # Creating Bot
 intents = discord.Intents.default()  # all default enabled intents
 intents.members = True  # Enabling priviliged intent "Members"
-owners = [231388394360537088]
+owners = [231388394360537088]  # Kadetino
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(config.prefix), owner_ids=set(owners), intents=intents)
 bot.remove_command('help')  # help command probably needs to be reworked
 
-# For Duels
+# For Duels and webhooks
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 bot.session = aiohttp.ClientSession()
 
