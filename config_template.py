0157c6d184740c8f46a431d2b145461e3db22197
Kade <89729954+Kadetino@users.noreply.github.com>
2022-04-08 22:17:29 +0300

    Update config_template.py

--- config_template.py
+++ config_template.py
@@ -2,3 +2,4 @@
 # token = ""  # Bot token
 # prefix="--"  # Bot prefix
 # cd_commands = 1  # Global cooldown
+# webhookJoinLeave = 'https://discord.com/api/webhooks/962053678980800582/QmBbtgjKZLf3zDoDEeKZIvXrONhrIr8sTWilVcWsPXxdrf6YdqJNOh0ONAYcOngVBZKP'
