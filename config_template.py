894eabaa0767bc5fc8afbb0e1ba81701f4f703a1
Kade <89729954+Kadetino@users.noreply.github.com>
2022-05-23 13:15:01 +0300

    Update config_template.py

--- config_template.py
+++ config_template.py
@@ -1,6 +1,10 @@
-# tesseract_cmd_path = ""  # path for OCR
-# token = ""  # Bot token
-# prefix="--"  # Bot prefix
+# # Default
+# owners = [231388394360537088]  # Kadetino
+# prefix = "--"  # Bot prefix
 # cd_commands = 1  # Global cooldown
-# webhookJoinLeave = 'https://discord.com/api/webhooks/962053678980800582/QmBbtgjKZLf3zDoDEeKZIvXrONhrIr8sTWilVcWsPXxdrf6YdqJNOh0ONAYcOngVBZKP'
-# webhookMPLobbies = 'https://discord.com/api/webhooks/962345589675294742/tOQsTg6n5wGqcErUYKN88-tqheOHNj3ufZbn_zYQPmipYp50ximrdsUB7-JyJ3512pBI'
+# cd_long_commands = 10  # Global long cooldown
+#
+# # Bot
+# token = ""
+# application_id =
+# webhookJoinLeave = ''
