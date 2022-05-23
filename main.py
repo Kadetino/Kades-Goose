8e54af8cd3a09c7fb6d44e37e1bd51cdde78ada7
Kade <89729954+Kadetino@users.noreply.github.com>
2022-05-23 13:14:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands  # Discord BOT
 import aiohttp  # For direct API requests and webhooks
 import warnings  # For direct API requests and webhooks
 
-from config import token, prefix, application_id, owners  # Global settings
+from config import token, prefix, application_id, owners, webhookJoinLeave  # Global settings
 
 
 # TODO Documentation / proper help command
@@ -14,11 +14,13 @@ class GooseBot(commands.Bot):
         intents = discord.Intents.default()
         intents.message_content = True
         intents.members = True
+
         # Constructor
         super().__init__(command_prefix=prefix, intents=intents, application_id=application_id,
                          owner_ids=set(owners))
         # Aiohttp
         self.session = aiohttp.ClientSession()
+
         # Cogs
         self.initial_extensions = [
             # "cogs.Lobby_finder_module",
@@ -48,6 +50,29 @@ class GooseBot(commands.Bot):
     async def on_ready(self):
         print('Logged on as {0.user}!'.format(bot))
         # await bot.change_presence(activity=discord.Game(name="Honk! Honk!"))
+
+    async def on_guild_join(self, guild):
+        """Send to goose server webhook, which servers bot has joined."""
+        # Webhook
+        webhook = discord.Webhook.from_url(webhookJoinLeave, session=aiohttp.ClientSession())
+        # Discord embed
+        server_embed = discord.Embed(title="New server!", description=f'{bot.user.name} has been added to: {guild}',
+                                     colour=discord.Colour.green())
+        server_embed.set_thumbnail(url=guild.icon)
+
+        return await webhook.send(embed=server_embed, username="Overseer")  # Executing webhook.
+
+    async def on_guild_remove(self, guild):
+        """Send to goose server webhook, which servers bot has left."""
+        # Webhook
+        webhook = discord.Webhook.from_url(webhookJoinLeave, session=aiohttp.ClientSession())
+        # Discord embed
+        server_embed = discord.Embed(title="Farewell...",
+                                     description=f'{bot.user.name} has left: {guild}',
+                                     colour=discord.Colour.red())
+        server_embed.set_thumbnail(url=guild.icon)
+
+        return await webhook.send(embed=server_embed, username="Overseer")  # Executing webhook.
 
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
